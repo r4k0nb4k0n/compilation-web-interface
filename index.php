17a18938744a1bcafab1ef44bccb3ce77e7a4499
Choe Hyeong Jin <chj6097@gmail.com>
2017-11-21 16:22:44 +0000

    Add codeMirror and use W3/CSS

--- index.php
+++ index.php
@@ -3,9 +3,12 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <script src="lib/codemirror.js"></script>
 <link rel="stylesheet" href="lib/codemirror.css">
-<script src="/mode/clike/clike.js"></script>
-<script src="/addon/selection/active-line.js"></script>
-<script src="/addon/edit/matchbrackets.js"></script>
+<script src="mode/clike/clike.js"></script>
+<script src="mode/shell/shell.js"></script>
+<link rel="stylesheet" href="theme/solarized.css">
+<script src="addon/selection/active-line.js"></script>
+<script src="addon/edit/matchbrackets.js"></script>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <head>
 	<title>Web Compiler</title>
 </head>
@@ -30,58 +33,83 @@
 		}
 		fclose($src); fclose($in);
 	?>
-	<h1>Web Compiler</h1>
+	<header class="w3-container w3-teal">
+  		<h1>Web Compilation Interface</h1>
+	</header>
 	
-	<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-		<h2>CODE</h2>
-		<textarea id="editor" name="editor"></textarea>
-		<h2>STDIN</h2>
-		<textarea id="stdin" name="stdin"></textarea>
+<div class="w3-cell-row">
+	<div class="w3-container w3-cell">
+			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+		<p>	
+			<h3>CODE<input class="w3-button w3-circle w3-teal" type="submit" name="submit "value="->">
+				<!--<input type="submit" name="submit" value="Compile">--></h3>
+			<textarea autofocus id="editor" name="editor"></textarea>
+		</p>
+		<p>
+		<!-- Print the err of the compilation -->
+	<h3>Status</h3>
+	<textarea readonly id="compile" name="compile"><?php echo $error;?></textarea></p>
+		</p>
+	</div>
 		<br>
-		<input type="submit" name="submit" value="컴파일 및 실행">
+		
 	</form>
+
+	<div class="w3-container w3-cell">
+ 		<p>
+		<h3>STDIN</h3>
+		<textarea id="stdin" name="stdin"></textarea>
+ 		<p><!-- Print the stdout of the program -->
+	<h3>STDOUT</h3>
+	<textarea id="stdout" name="stdout"><?php echo $stdout;?></textarea></p>
+	</div>
+
+</div>
 	
-	<!-- Print the err of the compilation -->
-	<h1>
-		Compile
-	</h1>
-	<textarea id="compile" name="compile"><?php echo $error;?></textarea>
 	
-	<!-- Print the stdout of the program -->
-	<h1>
-		stdout
-	</h1>
-	<textarea id="stdout" name="stdout"><?php echo $stdout;?></textarea>
 </body>
 </html>
 
-<script> 
+<script>
+	var w = window.innerWidth;
+	var h = window.innerHeight;
 	var textarea = document.getElementById('editor');
 	var editor = CodeMirror.fromTextArea(textarea, {
     	lineNumbers: true,
-   		lineWrapping: true,
+   		//lineWrapping: true,
     	matchBrackets: true,
-		smartIndent: true,
-		mode: "c++",
+		styleActiveLine: true,
+		smartIndent: false,
+		indentUnit: 4,
+		autofocus: true,
+		mode: "text/x-csrc",
     	theme: "solarized dark",
 		val: textarea.value
 	});
+	editor.setSize(w/2, h/3);
 	var textareain = document.getElementById('stdin');
 	var stdin = CodeMirror.fromTextArea(textareain, {
     	lineNumbers: true,
-   		lineWrapping: true,
-		val: textarea.value
+   		//lineWrapping: true,
+		styleActiveLine: true,
+		smartIndent: false,
+		indentUnit: 4,
+		val: textareain.value
 	});
+	stdin.setSize(w/2, h/3);
 	var textareaerr = document.getElementById('compile');
 	var compile = CodeMirror.fromTextArea(textareaerr, {
-    	lineNumbers: true,
-   		lineWrapping: true,
-		val: textarea.value
+    	readOnly: "nocursor",
+		mode: "text/x-sh",
+		val: textareaerr.value
 	});
+	compile.setSize(w/2, h/3);
 	var textareaout = document.getElementById('stdout');
 	var stdout = CodeMirror.fromTextArea(textareaout, {
     	lineNumbers: true,
-   		lineWrapping: true,
-		val: textarea.value
+   		//lineWrapping: true,
+		readOnly: true,
+		val: textareaout.value
 	});
+	stdout.setSize(w/2, h/3);
 </script>
--- src.c
+++ src.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#define MAX 20
+
 int main(){
-	printf("TEST IS GOOD...\n");
-    return 0;
+	char name[MAX];
+    int age;
+	printf("Input your name: ");
+    scanf("%s", name);
+    printf("Input your age: ");
+    scanf("%d", &age);
+    printf("\n\n");
+    printf("%s\n%d\n", name, age);
+   	return 0;
 }
