af425de6144041cb3553c22fa1b5eb58b06f3350
Choe Hyeong Jin <chj6097@gmail.com>
2017-11-22 04:04:43 +0000

    Change Directory structure

--- index.php
+++ index.php
@@ -1,74 +1,83 @@
 <meta charset="utf-8"/>
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<script src="lib/codemirror.js"></script>
-<link rel="stylesheet" href="lib/codemirror.css">
-<script src="mode/clike/clike.js"></script>
-<script src="mode/shell/shell.js"></script>
-<link rel="stylesheet" href="theme/solarized.css">
-<script src="addon/selection/active-line.js"></script>
-<script src="addon/edit/matchbrackets.js"></script>
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <head>
-	<title>Web Compiler</title>
+	<script src="extension/codemirror/lib/codemirror.js"></script>
+	<link rel="stylesheet" href="extension/codemirror/lib/codemirror.css">
+	<script src="extension/codemirror/mode/clike/clike.js"></script>
+	<script src="extension/codemirror/mode/shell/shell.js"></script>
+	<link rel="stylesheet" href="extension/codemirror/theme/solarized.css">
+	<script src="extension/codemirror/addon/selection/active-line.js"></script>
+	<script src="extension/codemirror/addon/edit/matchbrackets.js"></script>
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<title>Web Compilation Interface</title>
 </head>
 <body>
 	<?php
-		$codeErr = "";
 		$stdout = "";
 		$error = "";
 		$src = fopen("./src.c", "w+") or die("Unable to open file!");
-		$in = fopen("./in.txt", "w+") or die("Unable to open file!");
+		$stdin = fopen("./in.txt", "w+") or die("Unable to open file!");
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  			if (empty($_POST["editor"])) {
-    			$codeErr = "Code is required";
-  			} 
-			else {
-    			fwrite($src, $_POST["editor"]); fwrite($in, $_POST["stdin"]);
-				fclose($src); fclose($in);
-				$error = shell_exec("gcc -o prog src.c 2>&1");
+			if (isset($_POST["compile"])){
+				if (empty($_POST["editor"])) {
+    				$codeErr = "Code is required";
+  				} 
+				else {
+					shell_exec("rm ./prog");
+    				fwrite($src, $_POST["editor"]);
+					fclose($src); 
+					$error = shell_exec("gcc -o prog src.c 2>&1");
+				}
+			}
+  			else if(isset($_POST["execute"])){
+				fwrite($stdin, $_POST["stdin"]);
+				fclose($stdin);
 				$stdout = shell_exec("./prog < in.txt");
-				shell_exec("rm ./prog");
 			}
 		}
-		fclose($src); fclose($in);
+		fclose($src); fclose($stdin);
 	?>
+	
 	<header class="w3-container w3-teal">
   		<h1>Web Compilation Interface</h1>
 	</header>
 	
-<div class="w3-cell-row">
-	<div class="w3-container w3-cell">
+	<div class="w3-cell-row">
+		<div class="w3-container w3-cell">
 			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
-		<p>	
-			<h3>CODE<input class="w3-button w3-circle w3-teal" type="submit" name="submit "value="->">
-				<!--<input type="submit" name="submit" value="Compile">--></h3>
-			<textarea autofocus id="editor" name="editor"></textarea>
-		</p>
-		<p>
-		<!-- Print the err of the compilation -->
-	<h3>Status</h3>
-	<textarea readonly id="compile" name="compile"><?php echo $error;?></textarea></p>
-		</p>
-	</div>
-		<br>
-		
-	</form>
+			<p>	
+				<h3>CODE</h3>
+				<textarea autofocus id="editor" name="editor"><?php echo $src;?></textarea>
+			</p>
+			<p><!-- Print the status of the compilation -->
+				<h3>Status</h3>
+				<textarea readonly id="compile" name="compile"><?php echo $error;?></textarea></p>
+			</p>
+			<p>
+				<input class="w3-button w3-teal" type="submit" name="compile" value="Compile">
+			</p>
+			</form>
+		</div>	
 
-	<div class="w3-container w3-cell">
- 		<p>
-		<h3>STDIN</h3>
-		<textarea id="stdin" name="stdin"></textarea>
- 		<p><!-- Print the stdout of the program -->
-	<h3>STDOUT</h3>
-	<textarea id="stdout" name="stdout"><?php echo $stdout;?></textarea></p>
+		<div class="w3-container w3-cell">
+			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+ 			<p>
+				<h3>STDIN</h3>
+				<textarea id="stdin" name="stdin"><?php echo $stdin;?></textarea>
+			</p>
+ 			<p><!-- Print the stdout of the program -->
+				<h3>STDOUT</h3>
+				<textarea id="stdout" name="stdout"><?php echo $stdout;?></textarea>
+			</p>
+			<p>
+				<input class="w3-button w3-teal" type="submit" name="execute" value="Execute">
+			</p>
+			</form>
+		</div>
 	</div>
-
-</div>
-	
-	
-</body>
-</html>
+	</body>
+</html>	
 
 <script>
 	var w = window.innerWidth;
